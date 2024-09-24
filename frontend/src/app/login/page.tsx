5c1e06379e8a85619e83bbe0a2946478c2c70f2b
GabrielUzel <gabriel.uzel1@gmail.com>
2024-09-24 20:22:33 -0300

    Fix button and background image layout

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -7,45 +7,46 @@ import styles from '../../components/styles/loginPageStyle.module.css'
 
 export default function Login() {
     return (
-        <main className={styles.main}>
+        <>
             <NavBar />
-            <section className={`${styles.loginForm} ${styles.flex}`}>
-                <div className={`${styles.loginTtile} ${styles.flex}`}>
-                    <h1>Login</h1>
-                </div>
-                <div className={`${styles.loginMainFrame} ${styles.flex}`}>
-                    <LoginForm/>
-                    <div className={`${styles.loginEssentials} ${styles.flex}`}>
-                        <Link className={`${styles.googleLogo} ${styles.borderRadius10}`} href='/login/google'>
-                            <Image
-                                src = "/img/google-logo.svg"
-                                width = {28.71}
-                                height = {29.33}
-                                className = "logo"
-                                alt = "Logo image"
-                            />
-                            <p>
-                                Entrar com Google
-                            </p>
-                        </Link>
-                        <div className={`${styles.loginExtraLinks} ${styles.flex}`}>
-                            <p>
-                                Não tem uma conta? <Link className={styles.link} href='/signup'>Cadastre-se</Link>
-                            </p>
-                            <Link className={styles.link} href='/forgotpassword'>Esqueceu sua senha?</Link>
+            <main className={styles.main}>
+                <section className={`${styles.loginForm} ${styles.flex}`}>
+                    <div className={`${styles.loginTtile} ${styles.flex}`}>
+                        <h1 className='title'>Login</h1>
+                    </div>
+                    <div className={`${styles.loginMainFrame} ${styles.flex}`}>
+                        <LoginForm/>
+                        <div className={`${styles.loginEssentials} ${styles.flex}`}>
+                            <Link className={`${styles.googleLogo} ${styles.borderRadius10}`} href='/login/google'>
+                                <Image
+                                    src="/img/google-logo.svg"
+                                    width={28.71}
+                                    height={29.33}
+                                    alt="Logo image"
+                                />
+                                <p>
+                                    Entrar com Google
+                                </p>
+                            </Link>
+                            <div className={`${styles.loginExtraLinks} ${styles.flex}`}>
+                                <p>
+                                    Não tem uma conta? <Link className={styles.link} href='/signup'>Cadastre-se</Link>
+                                </p>
+                                <Link className={styles.link} href='/forgotpassword'>Esqueceu sua senha?</Link>
+                            </div>
                         </div>
                     </div>
+                </section>
+                <div className={styles.imageContainer}>
+                    <Image
+                        src="/img/template.jpeg"
+                        width={50}
+                        height={50}
+                        alt="Logo image"
+                        className={styles.backgroundImage}
+                    />
                 </div>
-            </section>
-            <div>
-                <Image
-                    src = "/img/template.jpeg"
-                    width = {30}
-                    height = {30}
-                    className = "logo"
-                    alt = "Logo image"
-                />
-            </div>
-        </main>
+            </main>
+        </>
     );
 }
