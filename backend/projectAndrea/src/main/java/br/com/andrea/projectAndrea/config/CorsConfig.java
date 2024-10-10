7fdeb1e993797ecce64eaacb6fef3ab8e5f30333
GabrielUzel <gabriel.uzel1@gmail.com>
2024-10-09 23:50:37 -0300

    Update dependencies

--- backend/projectAndrea/src/main/java/br/com/andrea/projectAndrea/config/CorsConfig.java
+++ backend/projectAndrea/src/main/java/br/com/andrea/projectAndrea/config/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
             .allowedOrigins("http://localhost:3000")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-            .allowedHeaders("Content-Type", "application/json");
+            .allowedHeaders("Content-Type", "Authorization");
     }
 }
 
