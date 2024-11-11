e11a247dd7908457dd3d8bb874767a67ea296518
GabrielUzel <gabriel.uzel1@gmail.com>
2024-11-11 15:03:36 -0300

    Implemented db search for profile picture

--- backend/projectAndrea/src/main/java/br/com/andrea/projectAndrea/repository/UserRepository.java
+++ backend/projectAndrea/src/main/java/br/com/andrea/projectAndrea/repository/UserRepository.java
@@ -1,11 +1,10 @@
 package br.com.andrea.projectAndrea.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 import br.com.andrea.projectAndrea.models.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
-    UserDetails findByEmail(String email);
+    User findByEmail(String email);
 }
--- frontend/src/components/profileImage.tsx
+++ frontend/src/components/profileImage.tsx
@@ -1,9 +1,31 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { CldImage } from 'next-cloudinary';
 import styles from '../styles/navBarStyle.module.css';
 
 export default function ProfileImage({width, height, toggleMenuState}) {
-    const [imageSrc] = useState('https://res.cloudinary.com/dy2mdjqn4/image/upload/v1730743526/project-andrea/default-user.svg');
+    const [imageSrc, setImageSrc] = useState('');
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+
+        fetch(`http://localhost:8080/auth/getprofilepicture`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+        }).then(response => response.json()
+        ).then(data => {
+            if(data.path != '#') {
+                setImageSrc(data.path);
+            } else {
+                setImageSrc('https://res.cloudinary.com/dy2mdjqn4/image/upload/v1730743526/project-andrea/default-user.svg');
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }, []);
 
     return(
         <CldImage
@@ -11,7 +33,7 @@ export default function ProfileImage({width, height, toggleMenuState}) {
             src={imageSrc}
             width={width}
             height={height}
-            alt="Image de perfil"
+            alt="Imagem de perfil"
             crop={{
                 type: 'auto',
                 source: true
