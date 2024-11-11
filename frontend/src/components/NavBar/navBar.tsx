b7ac6c91cd5c58943208f2bc4f60959114362269
GabrielUzel <gabriel.uzel1@gmail.com>
2024-11-11 13:34:04 -0300

    Update some lint warnings

--- frontend/src/components/NavBar/navBar.tsx
+++ frontend/src/components/NavBar/navBar.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useEffect, useLayoutEffect, useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import Link from 'next/link';
 import SearchBar from './searchBar';
 import BurguerMenu from './burguerMenu';
--- frontend/src/components/profileImage.tsx
+++ frontend/src/components/profileImage.tsx
@@ -3,14 +3,14 @@ import { CldImage } from 'next-cloudinary';
 import styles from '../styles/navBarStyle.module.css';
 
 export default function ProfileImage({width, height, toggleMenuState}) {
-    const [imageSrc, setImageSrc] = useState('https://res.cloudinary.com/dy2mdjqn4/image/upload/v1730743526/project-andrea/default-user.svg');
+    const [imageSrc] = useState('https://res.cloudinary.com/dy2mdjqn4/image/upload/v1730743526/project-andrea/default-user.svg');
 
     return(
         <CldImage
             className={styles.profileImage}
             src={imageSrc}
-            width="40"
-            height="40"
+            width={width}
+            height={height}
             alt="Image de perfil"
             crop={{
                 type: 'auto',
